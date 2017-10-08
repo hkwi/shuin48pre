36a1a4179e7509a185cdf3ec6e1f55325c3c22e5
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2017-10-08 23:56:49 +0900

    add kiboua

--- diffgen.py
+++ diffgen.py
@@ -239,6 +239,20 @@ def gray_to_koufuku():
 		fromfile="GrayDB", tofile="幸福公式", lineterm='\r\n')
 	open("docs/gray_to_koufuku.diff", "w").writelines(lines)
 
+def gray_to_kibou():
+	ks = "小選挙区 名前 前回 立候補".split()
+	db = [r+["党発表"] for r in csv.reader(open("docs/kibou_media.csv")) if not is_empty(r)]
+	
+	gk, gdb = load_gdoc("docs/gdoc_gray_db.csv")
+	gdb = [r for r in gdb if "希望" in r[gk.index("政党")]]
+	
+	keys = set(ks).intersection(set(gk))
+	
+	lines = difflib.unified_diff(ttl_out(gk, gdb, keys),
+		ttl_out(ks, db, keys),
+		fromfile="GrayDB", tofile="希望（報道）", lineterm='\r\n')
+	open("docs/gray_to_kibo.diff", "w").writelines(lines)
+
 
 if __name__=="__main__":
 	gray_to_seijinavi()
@@ -249,3 +263,4 @@ if __name__=="__main__":
 	gray_to_jimin()
 	gray_to_ritsumin()
 	gray_to_koufuku()
+	gray_to_kibou()
