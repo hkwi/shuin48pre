e502dffc71be347c7ea7b4fb7ba0e2c20ac7b464
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2017-10-08 22:40:27 +0900

    add koufuku official

---
+++ shuin48pre/koufuku_official.py
@@ -0,0 +1,34 @@
+import re
+import csv
+import lxml.html
+import urllib.request
+
+def run(fp):
+	fieldnames = "namel name_hira area hirei fb tw bl bio".split()
+	
+	out = csv.writer(fp)
+	d = lxml.html.parse(urllib.request.urlopen("https://candidates.hr-party.jp/elections/2017/1185/"))
+	ps = d.xpath('.//div[contains(@class,"p-y-1")]')
+	for p in ps:
+		r = dict(
+			name_hira = p.xpath(".//rb/text()"),
+			namel = p.xpath(".//rt/text()"),
+			area = p.xpath('.//p[@class="lead"]/text()'),
+			bio = p.xpath('.//p[@class="lead"]/following-sibling::p/text()'),
+			fb = p.xpath('.//i[contains(@class,"fa-facebook-square")]/parent::a/@href'),
+			tw = p.xpath('.//i[contains(@class,"fa-twitter-square")]/parent::a/@href'),
+			bl = p.xpath('.//i[contains(@class,"fa-pencil-square")]/parent::a/@href'),
+		)
+		for k,v in r.items():
+			r[k] = "".join(v).strip()
+		
+		r["name"] = re.sub("[　 ]", "", r["namel"])
+		if r["area"].startswith("比例"):
+			r["hirei"]=r["area"]
+			del(r["area"])
+		
+		out.writerow([r.get(i) for i in fieldnames])
+
+if __name__=="__main__":
+	import sys
+	run(sys.stdout)
